639b3096aa95bef5cf391e33712734479550066b
saik434455@gmail.com <saik434455@gmail.com>
2024-01-26 15:36:07 +0530

    Lottery builded

--- nextJs-lottery/backend/helper-hardhat-config.js
+++ nextJs-lottery/backend/helper-hardhat-config.js
@@ -30,8 +30,9 @@ const networkConfig = {
 
 const developmentChains = ["hardhat", "localhost"]
 const VERIFICATION_BLOCK_CONFIRMATIONS = 6
-const frontEndContractsFile = "../nextjs-smartcontract-lottery-fcc/constants/contractAddresses.json"
-const frontEndAbiFile = "../nextjs-smartcontract-lottery-fcc/constants/abi.json"
+const frontEndContractsFile = "../frontend/constants/contractAddress.json"
+const frontEndAbiFile = "../frontend/constants/abi.json"
+
 
 module.exports = {
     networkConfig,
